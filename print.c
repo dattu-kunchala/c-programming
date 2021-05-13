6ab6a8a379c4176b612b1f873b71ea39cae83cc6
dattu-kunchala <84142746+dattu-kunchala@users.noreply.github.com>
2021-05-13 21:22:18 +0530

    print uploaded
    printing problems uploaded

---
+++ print.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+void main()
+{
+printf("WOW big deal\n");
+}
+
---
+++ print2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+printf("version1\t");
+printf("The %d Wise Men\n",3);
+printf("version 2\t");
+printf(" the 3 wise men\n");
+/*
+echo version2;
+echo The3WiseMen;
+*/
+echo 'hello';
+}
+
